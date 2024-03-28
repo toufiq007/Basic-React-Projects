90c130fd81a7db403d09046f896c415f62849f2d
toufiq007 <t.limon2342@gmail.com>
2024-03-29 01:08:27 +0600

    remove previous code from countDown timer

--- project-7-countdown-timer/countdown_timer_ts/src/components/InputTimer.tsx
+++ project-7-countdown-timer/countdown_timer_ts/src/components/InputTimer.tsx
@@ -1,37 +1,3 @@
-// const InputTimer = ({ handleChange, handleStart }) => {
-//   return (
-//     <div className="">
-//       <div className="inputContainer mb-9">
-//         <input
-//           onChange={(e) => handleChange(e)}
-//           id="hours"
-//           type="text"
-//           placeholder="HH"
-//           className=" text-7xl text-center mx-3 border-4 w-[200px]"
-//         />
-//         <input
-//           onChange={(e) => handleChange(e)}
-//           id="minutes"
-//           type="text"
-//           placeholder="MM"
-//           className=" text-7xl text-center mx-3 border-4 w-[200px]"
-//         />
-//         <input
-//           onChange={(e) => handleChange(e)}
-//           id="seconds"
-//           type="text"
-//           placeholder="SS"
-//           className=" text-7xl text-center mx-3 border-4 w-[200px]"
-//         />
-//       </div>
-//       <button onClick={handleStart} className="text-slate-300 w-[150px]">
-//         Start
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default InputTimer;
 
 type InputTimerProps = {
   handleStart: () => void;
